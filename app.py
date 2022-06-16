398ff0409bfa8518918e68a868a781131bf7b0c3
Armen-ar <armen.aruts1967@gmail.com>
2022-06-16 20:16:23 +0300

    initial

--- app.py
+++ app.py
@@ -1,40 +1,109 @@
-# app.py
-
 from flask import Flask, request
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
+from create_data import Movie
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 3}
 db = SQLAlchemy(app)
 
-
-class Movie(db.Model):
-    __tablename__ = 'movie'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    trailer = db.Column(db.String(255))
-    year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
-    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    genre = db.relationship("Genre")
-    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    director = db.relationship("Director")
-
-class Director(db.Model):
-    __tablename__ = 'director'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+api = Api(app)
+movie_ns = api.namespace('movies')
 
 
-class Genre(db.Model):
-    __tablename__ = 'genre'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
 
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+@movie_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        all_movies = db.session.query(Movie).all()
+
+        return movies_schema.dump(all_movies), 200
+
+    def post(self):
+        req_json = request.json
+        new_movie = Movie(**req_json)
+
+        with db.session.begin():
+            db.session.add(new_movie)
+
+            return "", 201
+
+
+@movie_ns.route('/<int:bid>')
+class MoviesView(Resource):
+    def get(self, bid: int):
+        try:
+            movie = db.session.query(Movie).filter(Movie.id == bid).one()
+            return movie_schema.dump(movie), 200
+        except Exception as e:
+            return str(e), 404
+
+    def put(self, bid: int):
+        movie = db.session.query(Movie).get(bid)
+        req_json = request.json
+
+        movie.title = req_json.get("title")
+        movie.description = req_json.get("description")
+        movie.trailer = req_json.get("trailer")
+        movie.year = req_json.get("year")
+        movie.rating = req_json.get("rating")
+        movie.genre_id = req_json.get("genre_id")
+        movie.director_id = req_json.get("director_id")
+
+        db.session.add(movie)
+        db.session.commit()
+
+        return "", 204
+
+    def patch(self, bid: int):
+        movie = db.session.query(Movie).get(bid)
+        req_json = request.json
+
+        if "title" in req_json.get:
+            movie.title = req_json.get("title")
+        if "description" in req_json.get:
+            movie.description = req_json.get("description")
+        if "trailer" in req_json.get:
+            movie.trailer = req_json.get("trailer")
+        if "year" in req_json.get:
+            movie.year = req_json.get("year")
+        if "rating" in req_json.get:
+            movie.rating = req_json.get("rating")
+        if "genre_id" in req_json.get:
+            movie.genre_id = req_json.get("genre_id")
+        if "director_id" in req_json.get:
+            movie.director_id = req_json.get("director_id")
+
+        db.session.add(movie)
+        db.session.commit()
+
+        return "", 204
+
+    def delete(self, bid: int):
+        movie = db.session.query(Movie).get(bid)
+
+        db.session.delete(movie)
+        db.session.commit()
+
+        return "", 204
 
 
 if __name__ == '__main__':
